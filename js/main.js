39a295887073ff132d386d25cf1568d8a7f6845b
Stepan Pribytko <49967730+stepanpr@users.noreply.github.com>
2020-01-26 11:11:24 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,16 @@
+new WOW().init();
+// var mySwiper = new Swiper('.swiper-container', {
+//     speed: 400,
+//     spaceBetween: 100
+// });
+
+var mySwiper = new Swiper ('.swiper-container', {
+    // If we need pagination
+    pagination: {
+      el: '.projects-pagination',
+      bulletClass: 'projects-bullet',
+      bulletActiveClass: 'projects-bullet-active',
+      clickable: true
+    },
+   
+  })
